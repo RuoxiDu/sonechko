566cf9c4ae25696bc21bd900f09cb524daa67289
RuoxiDu <du.ruoxi.1988@gmail.com>
2019-07-09 14:44:25 -0400

    Ruoxi - change icon list

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ListPage implements OnInit {
   private selectedItem: any;
   private icons = [
-   'bicycle'
+   'baseball'
   ];
   public items: Array<{ title: string; note: string; icon: string }> = [];
   constructor() {
